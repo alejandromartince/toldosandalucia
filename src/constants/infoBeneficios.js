eb4f27d580522e2eec60b6bc93c8f49de7cead94
alejandromartince <alejandromartin.ro.ce@gmail.com>
2025-03-24 12:33:08 +0100

    Anadimos la primera carta que hara de ejemplo para el resto de cosas de beneficios

--- src/constants/infoBeneficios.js
+++ src/constants/infoBeneficios.js
@@ -1,35 +1,35 @@
-import { TfiRulerPencil } from "react-icons/tfi";
-import { GiFactoryArm } from "react-icons/gi";
-import { GrUserWorker } from "react-icons/gr";
-import { TbRulerMeasure } from "react-icons/tb";
-
 export const tituloBeneficio = {
   es: "Lo que nos diferencia",
   en: "What sets us apart",
 };
 
+export const subtituloBeneficio = {
+  es: "Gracias a nuestra amplia experiencia, hemos perfeccionado nuestras herramientas y optimizado cada aspecto de nuestro trabajo, lo que nos ha permitido posicionarnos como líderes en el sector.",
+  en: "Thanks to our extensive experience, we have refined our tools and optimized every aspect of our work, allowing us to position ourselves as leaders in the industry.",
+}
+
 export const textoBeneficio = {
   es: {
     medicion: {
-      icono: <TbRulerMeasure size={60} />,
+      icono: "medicion", // Usamos solo el nombre
       titulo: "Medición del espacio",
       informacion:
         "Tomamos medidas precisas del espacio donde se instalará el toldo, asegurando un ajuste perfecto y una instalación sin inconvenientes.",
     },
     diseno: {
-      icono: <TfiRulerPencil size={60} />,
+      icono: "diseno", // Usamos solo el nombre
       titulo: "Diseños personalizados",
       informacion:
         "Diseñamos e instalamos toldos a medida, tanto manuales como automatizados, integrando soluciones de domótica. Además, ofrecemos el servicio de rotulación para todo tipo de toldos.",
     },
     fabricacion: {
-      icono: <GiFactoryArm size={60} />,
+      icono: "fabricacion", // Usamos solo el nombre
       titulo: "Fabricación de toldos",
       informacion:
         "Nuestro equipo altamente cualificado instala el toldo asegurando su correcta fijación y funcionalidad, garantizando un trabajo profesional adaptado a cada proyecto.",
     },
     instalacion: {
-      icono: <GrUserWorker size={60} />,
+      icono: "instalacion", // Usamos solo el nombre
       titulo: "Instalación y mantenimiento",
       informacion:
         "Nuestro equipo altamente cualificado instala el toldo asegurando su correcta fijación y funcionalidad, garantizando un trabajo profesional adaptado a cada proyecto.",
@@ -38,25 +38,25 @@ export const textoBeneficio = {
 
   en: {
     medicion: {
-      icono: <TfiRulerPencil size={60} />,
+      icono: "medicion", // Usamos solo el nombre
       titulo: "Space measurement",
       informacion:
         "We take precise measurements of the space where the awning will be installed, ensuring a perfect fit and trouble-free installation.",
     },
     diseno: {
-      icono: <TfiRulerPencil size={60} />,
+      icono: "diseno", // Usamos solo el nombre
       titulo: "Custom designs",
       informacion:
         "We design and install custom awnings, both manual and automated, integrating smart home solutions. Additionally, we offer signage services for all types of awnings.",
     },
     fabricacion: {
-      icono: <GrUserWorker size={60} />,
+      icono: "fabricacion", // Usamos solo el nombre
       titulo: "Awning manufacturing",
       informacion:
         "Our highly qualified team installs the awning, ensuring its proper fixation and functionality, guaranteeing a professional job tailored to each project.",
     },
     instalacion: {
-      icono: <GrUserWorker size={60} />,
+      icono: "instalacion", // Usamos solo el nombre
       titulo: "Installation and maintenance",
       informacion:
         "Our highly qualified team installs the awning, ensuring its proper fixation and functionality, guaranteeing a professional job tailored to each project.",
--- src/sections/Beneficios/Beneficios.jsx
+++ src/sections/Beneficios/Beneficios.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
+//Importamos los componentes
+
 // Importamos la información
 import {
   tituloBeneficio,
+  subtituloBeneficio,
 } from "../../constants/infoBeneficios";
 
 // Importamos los contextos
 import { useIdioma } from "../../contexts/IdiomaContext";
 
 import "./Beneficios.css";
+import CartaBeneficio from "../../components/CartaBeneficio";
 
 const Beneficios = () => {
   const { idioma } = useIdioma();
@@ -19,6 +23,19 @@ const Beneficios = () => {
         <h1>{tituloBeneficio[idioma]}</h1>
         <br />
         <hr className="hr-beneficios" />
+        <br />
+        <p>{subtituloBeneficio[idioma]}</p>
+      </div>
+
+      <br />
+
+      
+
+      <div className="grid-beneficios">
+        <div><CartaBeneficio /></div>
+        <div>adios</div>
+        <div>hola</div>
+        <div>adios</div>
       </div>
     </section>
   );
