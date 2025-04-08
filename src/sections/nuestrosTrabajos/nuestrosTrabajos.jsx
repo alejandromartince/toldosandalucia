9ab2cd0c81c6d2f811111db2f370dfa1206ce240
alejandromartince <alejandromartin.ro.ce@gmail.com>
2025-04-08 15:01:30 +0200

    Eliminamos codigo redundante de nuestrostrabajos

--- src/sections/nuestrosTrabajos/nuestrosTrabajos.jsx
+++ src/sections/nuestrosTrabajos/nuestrosTrabajos.jsx
@@ -1,20 +1,10 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectCoverflow, Navigation, Pagination } from "swiper/modules";
 
 //Importamos los estilos obligatorios para que el swiper funcione
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-
-//Importamos la informacion
-import {
-  infoNuestrosTrabajos,
-} from "../../constants/infoNuestrosTrabajos";
-
-//Importamos los contextos
-import { useIdioma } from "../../contexts/IdiomaContext";
 
 //Importamos los componentes
 import CarruselTrabajos from "../../components/carrouselTrabajos";
@@ -23,9 +13,7 @@ import CarruselTrabajos from "../../components/carrouselTrabajos";
 import "./NuestrosTrabajos.css"; // Archivo de estilos
 
 const NuestrosTrabajos = () => {
-  const informacion = infoNuestrosTrabajos;
-  const { idioma } = useIdioma();
-
+ 
   return (
     <section className="nuestrosTrabajos-section" id="works">
         <CarruselTrabajos />
