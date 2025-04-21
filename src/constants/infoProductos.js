1a7f6c3da212ac13164dc04b0bd351a01d435e64
alejandromartince <alejandromartin.ro.ce@gmail.com>
2025-04-21 19:47:26 +0200

    Cambiamos las descripciones de los articulos

--- src/constants/infoProductos.js
+++ src/constants/infoProductos.js
@@ -12,8 +12,8 @@ export const Productos = [
       en: "Terrace",
     },
     descripcion: {
-      es: "Los toldos para terraza son la solución ideal para proteger del sol y crear un ambiente confortable y elegante en cualquier espacio exterior.",
-      en: "Terrace awnings are the perfect solution for sun protection, creating a comfortable and stylish outdoor space.",
+      es: "Los toldos para terraza son la solución ideal para proteger del sol y crear un ambiente confortable y elegante en cualquier espacio exterior. Además de brindar sombra, ayudan a mantener una temperatura agradable, permiten disfrutar del aire libre durante más tiempo y aportan un toque estético que mejora la apariencia del entorno.",
+      en: "Terrace awnings are the ideal solution for protecting against the sun while creating a comfortable and stylish atmosphere in any outdoor space. In addition to providing shade, they help maintain a pleasant temperature, allow you to enjoy the outdoors for longer periods, and add an aesthetic touch that enhances the look of the surroundings.",
     },
     position: [0, 0, 0], // Aquí agregamos la posición para este producto
     escala: 1.3, //Lo mismo con la escala
@@ -25,10 +25,10 @@ export const Productos = [
       en: "Veranda",
     },
     descripcion: {
-      es: "Los toldos veranda son la solución perfecta para cubrir techos acristalados y buhardillas, ofreciendo protección solar eficiente y reduciendo la acumulación de calor en el interior.",
-      en: "Veranda awnings are the perfect solution for covering glass roofs and attics, providing effective sun protection and reducing heat buildup indoors.",
+      es: "Los toldos veranda son la solución perfecta para cubrir techos acristalados y buhardillas, ofreciendo una protección solar eficiente y reduciendo la acumulación de calor en el interior. Este tipo de toldo permite aprovechar la luz natural sin renunciar al confort térmico, mejorando tanto la eficiencia energética como el bienestar dentro del hogar o espacio cubierto.",
+      en: "Veranda awnings are the perfect solution for covering glass roofs and attic spaces, providing efficient sun protection and significantly reducing heat buildup inside. This type of awning allows you to enjoy natural light without sacrificing thermal comfort, enhancing both energy efficiency and the overall comfort of your home or covered area.",
     },
-    position: [-2, -1.2, 0.9], 
+    position: [-2, -1.2, 0.9],
     escala: .8
   },
   // Puedes agregar más productos aquí si lo deseas
