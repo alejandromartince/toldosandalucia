e1bcc4a6374b851264b6d76fb69713bf591ec78c
alejandromartince <alejandromartin.ro.ce@gmail.com>
2025-05-11 19:24:58 +0200

    Añadimos los iconos al footer

--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -6,6 +6,11 @@ import { infoFooter } from "../../constants/infoFooter";
 //Importamos el contexto del idioma
 import { useIdioma } from "../../contexts/IdiomaContext";
 
+//Importamos los iconos
+import { FaTiktok } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+
 //Importamos los estilos
 import "./Footer.css";
 
@@ -35,10 +40,37 @@ const Footer = () => {
           <p>{informacion[idioma].ayuda}</p>
         </div>
         <div className="info-redes">
-          <p>redes</p>
-          <span>
-            <hr />
-          </span>
+          <div className="info-redes-titulo">
+            <h2>{informacion[idioma].titulos.redes}</h2>
+            <span>
+              <hr />
+            </span>
+          </div>
+          <div>
+            <a
+              href="https://www.facebook.com/profile.php?id=100069359261072&mibextid=wwXIfr&rdid=TFKKyCeTM9mYAP5T&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F1BV9Xfr3Y1%2F%3Fmibextid%3DwwXIfr#"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebookF size={20} className="icon-redondo" color="white"/>
+            </a>
+
+            <a
+              href="https://www.tiktok.com/@toldos.andalucia"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTiktok size={20} className="icon-redondo" color="white"/>
+            </a>
+
+            <a
+              href="https://www.instagram.com/toldosandalucia/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram size={20} className="icon-redondo" color="white"/>
+            </a>
+          </div>
         </div>
         <div className="logo">
           <img
