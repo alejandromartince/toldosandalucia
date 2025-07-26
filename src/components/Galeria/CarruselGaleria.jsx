2a22dcefe6106d8032546d8007807e8889d48722
alejandromartince <alejandromartin.ro.ce@gmail.com>
2025-07-27 01:32:45 +0200

    STYLE: empezamos a editar la galeria

--- src/components/Galeria/CarruselGaleria.jsx
+++ src/components/Galeria/CarruselGaleria.jsx
@@ -1,34 +1,58 @@
 // CarruselGaleria.jsx
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper/modules";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 
 const CarruselGaleria = ({ infoTrabajos, setIndiceActual, indiceActual }) => {
-    return (
-        <Swiper
-            modules={[Navigation]}
-            navigation
-            loop={true}
-            slidesPerView={4}
-            slidesPerGroup={1}
-            onSlideChange={(swiper) => setIndiceActual(swiper.realIndex)}
-            className="swiper-galeria"
-        >
-            {infoTrabajos.map((trabajo, index) => (
-                <SwiperSlide key={index}>
-                    <div className={`galeria-slide ${indiceActual === index ? 'slide-activo' : ''}`}>
-                        <img
-                            src={trabajo.imagen}
-                            alt={trabajo.titulo?.es || `Trabajo ${index + 1}`}
-                            className="galeria-imagen"
-                        />
-                    </div>
-                </SwiperSlide>
-            ))}
-        </Swiper>
-    );
+  return (
+    <Swiper
+      modules={[Navigation, Pagination, Autoplay]}
+      navigation
+      loop={true}
+      autoplay={{ delay: 3000, disableOnInteraction: false }}
+      pagination={false}
+      direction="horizontal"
+      simulateTouch={false}
+      onSlideChange={(swiper) => setIndiceActual(swiper.realIndex)}
+      spaceBetween={"4rem"}
+      className="swiper-galeria"
+      breakpoints={{
+        0: {
+          slidesPerView: 1,
+          slidesPerGroup: 1,
+        },
+        768: {
+          slidesPerView: 2,
+          slidesPerGroup: 1,
+        },
+        1024: {
+          slidesPerView: 3,
+          slidesPerGroup: 1,
+        },
+        1450: {
+          slidesPerView: 4,
+          slidesPerGroup: 1,
+        },
+      }}
+    >
+      {infoTrabajos.map((trabajo, index) => (
+        <SwiperSlide key={index}>
+          <div
+            className={`galeria-slide ${
+              indiceActual === index ? "slide-activo" : ""
+            }`}
+          >
+            <img
+              src={trabajo.imagen}
+              alt={trabajo.titulo?.es || `Trabajo ${index + 1}`}
+              className="galeria-imagen"
+            />
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 };
-
 
 export default CarruselGaleria;
--- src/sections/galeria/Galeria.jsx
+++ src/sections/galeria/Galeria.jsx
@@ -1,57 +1,100 @@
 //Importamos los hooks de react
-import { useState } from 'react';
+import { useState } from "react";
 
 //Importamos la información
-import { imagesNuestrosTrabajos } from '../../constants/infoNuestrosTrabajos.js'
+import { imagesNuestrosTrabajos } from "../../constants/infoNuestrosTrabajos.js";
 
 //Importamos los hooks personales
-import useTipoDipositivo from "../../Hooks/useTipoDispositivo.js"
+import useTipoDipositivo from "../../Hooks/useTipoDispositivo.js";
 
 //Importamos los componentes
-import CarruselGaleria from '../../components/Galeria/CarruselGaleria.jsx';
+import CarruselGaleria from "../../components/Galeria/CarruselGaleria.jsx";
 
 //Importamos el contexto del idioma
-import { useIdioma } from '../../Hooks/General/useIdioma.js'
+import { useIdioma } from "../../Hooks/General/useIdioma.js";
 
 //Importamos el estilo
-import './Galeria.css'
+import "./Galeria.css";
 
 const Galeria = () => {
+  const dispositivo = useTipoDipositivo();
+  const [indiceActual, setIndiceActual] = useState(0);
+  const { idioma } = useIdioma();
 
-    const dispositivo = useTipoDipositivo();
-    const [indiceActual, setIndiceActual] = useState(0);
-    const { idioma } = useIdioma();
-    const imagenActual = imagesNuestrosTrabajos[indiceActual];
+  const textos = () => {
+    switch (idioma) {
+      case "es":
+        return {
+          primerTexto: "Para más información, contacte con nosotros ",
+          segundoTexto: "aquí",
+        };
+      case "en":
+        return {
+          primerTexto: "For more information, contact us ",
+          segundoTexto: "here",
+        };
+      case "fr":
+        return {
+          primerTexto: "Pour plus d'informations, contactez-nous ",
+          segundoTexto: "ici",
+        };
+      case "de":
+        return {
+          primerTexto: "Für weitere Informationen kontaktieren Sie uns ",
+          segundoTexto: "hier",
+        };
+      case "da":
+        return {
+          primerTexto: "For mere information, kontakt os ",
+          segundoTexto: "her",
+        };
+      case "ru":
+        return {
+          primerTexto:
+            "Для получения дополнительной информации свяжитесь с нами ",
+          segundoTexto: "здесь",
+        };
+      default:
+        return {
+          primerTexto: "For more information, contact us ",
+          segundoTexto: "here",
+        };
+    }
+  };
 
-    return (
+  const { primerTexto, segundoTexto } = textos();
+  const imagenActual = imagesNuestrosTrabajos[indiceActual];
 
-        <section className='galeria-section'>
-            <div className='backgroundTopGaleria'>
-                <div className='contenidoGaleria'>
-                    <h2>{imagenActual.titulo[idioma]}</h2>
-                    <p>
-                        {idioma === 'es' ? (
-                            <>
-                                Para más información, contacte con nosotros <a href={dispositivo !== 'movil' ? "https://web.whatsapp.com/send?phone=34679847618&text=" : "https://wa.me/34679847618"} target='_blank'><u>aquí</u></a>
-                            </>
-                        ) : (
-                            <>
-                                For more information, contact us <a href={dispositivo !== 'movil' ? "https://web.whatsapp.com/send?phone=34679847618&text=" : "https://wa.me/34679847618"}><u>here</u></a>
-                            </>
-                        )}
-                    </p>
-                </div>
-            </div>
+  return (
+    <section className="galeria-section">
+      <div className="backgroundTopGaleria">
+        <div className="contenidoGaleria">
+          <h2>{imagenActual.titulo[idioma]}</h2>
+          <p style={{ textShadow: "none" }}>
+            {primerTexto}
+            <a
+              href={
+                dispositivo !== "movil"
+                  ? "https://web.whatsapp.com/send?phone=34679847618&text="
+                  : "https://wa.me/34679847618"
+              }
+              style={{ textDecoration: "underline" }}
+            >
+              {segundoTexto}
+            </a>
+          </p>
+        </div>
+      </div>
 
-            <div className='carrusel-galeria-container'>
-                <CarruselGaleria
-                    infoTrabajos={imagesNuestrosTrabajos}
-                    setIndiceActual={setIndiceActual}
-                    indiceActual={indiceActual}
-                />
-            </div>
-        </section>
-    );
+      <div className="carrusel-galeria-container">
+        <CarruselGaleria
+          infoTrabajos={imagesNuestrosTrabajos}
+          setIndiceActual={setIndiceActual}
+          indiceActual={indiceActual}
+        />
+      </div>
+    </section>
+  );
 };
 
-export default Galeria
+export default Galeria;
